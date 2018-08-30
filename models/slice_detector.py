(1) fixed computation of miscalibration plots; (2) changed classification metric plots

models/slice_detector.py
@@ -100,7 +100,8 @@ class DegenerateSliceDetector(nn.Module):
 
         features = self.base_model.features(x_in)
         if keep_features:
-            self.features = features
+            # keep features but convert to numpy array and fetch from GPU
+            self.features = features.data.cpu().numpy()
         y = self.base_model.classifier(features)
         # we return softmax probs and log-softmax. The last for computation of NLLLoss
         out = {"softmax": self.softmax_layer(y),
@@ -134,8 +135,8 @@ class DegenerateSliceDetector(nn.Module):
             b_loss = torch.mean(b_loss, dim=0)
         return b_loss
 
-    def do_forward_pass(self, x_input, y_labels):
-        out = self(x_input)
+    def do_forward_pass(self, x_input, y_labels, keep_features=False):
+        out = self(x_input, keep_features=keep_features)
         loss = self.get_loss(out, y_labels, average=False)
 
         return loss, out["softmax"]

plotting/calibration_plots.py
@@ -72,6 +72,8 @@ def compute_calibration_terms(exper_dict, patient_id=None, mc_dropout=False, for
     # acc_bins: accuracy per bin. numerator="# of positive predicted class labels for this bin";
     #                             denominator="# of predictions in this bin"
     acc_bins = np.zeros((2, 4, num_of_bins))
+    probs_per_bin = np.zeros((2, 4, num_of_bins))
+    probs_per_bin_denom = np.zeros((2, 4, num_of_bins))
     acc_bins_used = np.zeros((2, 4, num_of_bins))
     mean_counts_per_bin = np.zeros((2, 4, num_of_bins))
     # confidence measure = mean predicted probability in this bin
@@ -110,10 +112,16 @@ def compute_calibration_terms(exper_dict, patient_id=None, mc_dropout=False, for
                         # get all predicted probabilities that "predicted" correctly the pos-class label
                         pred_probs_cls_cor = pred_probs_cls[pos_voxels_idx]
                         counts_pred_probs, _ = np.histogram(pred_probs_cls, bins=prob_bins)
+                        cls_idx_probs_per_bin = np.digitize(pred_probs_cls, bins=prob_bins)
                         # bin the predicted probabilities with correct predictions (class labels)
                         counts_pred_probs_cor, _ = np.histogram(pred_probs_cls_cor, bins=prob_bins)
                         acc_per_bin = np.zeros(num_of_bins)
                         for bin_idx in np.arange(len(acc_per_bin)):
+                            # do the stuff to compute the conf(B_m) metric from the Guo & Pleiss paper
+                            cls_probs_per_bin = pred_probs_cls[cls_idx_probs_per_bin == bin_idx + 1]
+                            probs_per_bin[phase, cls_idx, bin_idx] += np.sum(cls_probs_per_bin)
+                            probs_per_bin_denom[phase, cls_idx, bin_idx] += cls_probs_per_bin.shape[0]
+                            # do the stuff to compute the acc(B_m) metric from the Guo&Pleiss paper
                             if counts_pred_probs[bin_idx] != 0:
                                 acc_per_bin[bin_idx] = counts_pred_probs_cor[bin_idx] * 1./counts_pred_probs[bin_idx]
                                 acc_bins[phase, cls_idx, bin_idx] += acc_per_bin[bin_idx]
@@ -134,6 +142,8 @@ def compute_calibration_terms(exper_dict, patient_id=None, mc_dropout=False, for
             for bin_idx in np.arange(len(prob_bins[1:])):
                 if acc_bins_used[phase, cls_idx, bin_idx] != 0:
                     acc_bins[phase, cls_idx, bin_idx] *= 1./acc_bins_used[phase, cls_idx, bin_idx]
+                if probs_per_bin_denom[phase, cls_idx, bin_idx] != 0:
+                    probs_per_bin[phase, cls_idx, bin_idx] *= 1./probs_per_bin_denom[phase, cls_idx, bin_idx]
 
     # compute final mean ECE value per class, omit the BACKGROUND class
     mean_ece_per_class = np.nan_to_num(np.divide(ece_per_class[:, 1:], ece_counts_per_class[:, 1:]))
@@ -144,14 +154,16 @@ def compute_calibration_terms(exper_dict, patient_id=None, mc_dropout=False, for
             e_suffix = ""
             if with_bg:
                 e_suffix = "_wbg"
-            file_name = "calibration_" + exper_args.model + "_" + exper_args.loss_function + e_suffix + ".npz"
+            loss_function = exper_args.loss_function.replace("-", "")
+            file_name = "calibration_" + exper_args.model + "_" + loss_function + e_suffix + ".npz"
             file_name = os.path.join(config.data_dir, file_name)
-            np.savez(file_name, prob_bins=prob_bins, acc_bins=acc_bins, mean_ece_per_class=mean_ece_per_class)
+            np.savez(file_name, prob_bins=prob_bins, acc_bins=acc_bins, mean_ece_per_class=mean_ece_per_class,
+                     probs_per_bin=probs_per_bin)
             print("INFO - Successfully saved numpy arrays to location {}".format(file_name))
         except IOError:
             raise IOError("ERROR - can't save numpy arrays to location {}".format(file_name))
 
-    return prob_bins, acc_bins, mean_ece_per_class
+    return prob_bins, acc_bins, mean_ece_per_class, probs_per_bin
 
 
 def plot_reliability_diagram(cal_data, height=None, width=16, do_show=True, do_save=False, per_class=False):
@@ -184,6 +196,7 @@ def plot_reliability_diagram(cal_data, height=None, width=16, do_show=True, do_s
         the_range = np.arange(0, 1)
         # average per phase over classes ignoring background class
         acc_bins = np.mean(cal_data.acc_per_bin[:, 1:], axis=1)
+        probs_per_bin = np.mean(cal_data.probs_per_bin[:, 1:], axis=1)
 
     row = 0
     bar_width = 0.09
@@ -208,7 +221,8 @@ def plot_reliability_diagram(cal_data, height=None, width=16, do_show=True, do_s
             # print(acc_ed)
             ax1.set_title("{} ".format(phase_labels[1]), **sub_title_size)
         # compute the gap between fraction of forcast and identiy. Set to zero if bin was empty (2nd line)
-        y_gaps = np.abs(prob_bins[1:] - acc_ed)
+        # old version how we computed miscalibration (wrong): y_gaps = np.abs(prob_bins[1:] - acc_ed)
+        y_gaps = np.abs(probs_per_bin[1] - acc_ed)
         y_gaps[y_gaps == prob_bins[1:]] = 0
         ax1.bar(prob_bins[1:], acc_ed, bar_width, color="b", alpha=0.4)
         ax1.bar(prob_bins[1:], y_gaps, bar_width, bottom=acc_ed, color="r", alpha=0.3, hatch='/',
@@ -231,7 +245,8 @@ def plot_reliability_diagram(cal_data, height=None, width=16, do_show=True, do_s
             # print(acc_es)
             ax2.set_title("{} ".format(phase_labels[0]), **sub_title_size)
         # compute the gap between fraction of forcast and identiy. Set to zero if bin was empty (2nd line)
-        y_gaps = np.abs(prob_bins[1:] - acc_es)
+        # old version how we computed miscalibration (wrong): y_gaps = np.abs(prob_bins[1:] - acc_es)
+        y_gaps = np.abs(probs_per_bin[0] - acc_es)
         y_gaps[y_gaps == prob_bins[1:]] = 0
         ax2.bar(prob_bins[1:], acc_es, bar_width, color="g", alpha=0.4)  # yerr=y_gaps,
         ax2.bar(prob_bins[1:], y_gaps, bar_width, bottom=acc_es, color="r", alpha=0.3, label="Miscalibration",
@@ -306,10 +321,12 @@ class CalibrationData(object):
         self.prob_bin_edges = None
         self.acc_per_bin = None
         self.mean_ece_per_class = None
+        # is actually equal to conf(B_m) metric from Guo & Pleiss paper, has shape [2, 4, #bins]
+        self.probs_per_bin = None
 
     def load(self):
-
-        file_name = CalibrationData.file_prefix + self.model_name + "_" + self.loss_function + ".npz"
+        loss_function = self.loss_function.replace("-", "")
+        file_name = CalibrationData.file_prefix + self.model_name + "_" + loss_function + ".npz"
         file_name = os.path.join(config.data_dir, file_name)
         if len(glob.glob(file_name)) != 1:
             raise ValueError("ERROR - found {} files with name {}. Must be one".format(len(glob.glob(file_name)),
@@ -320,6 +337,7 @@ class CalibrationData(object):
             self.prob_bin_edges = data["prob_bins"]
             self.acc_per_bin = data["acc_bins"]
             self.mean_ece_per_class = data["mean_ece_per_class"]
+            self.probs_per_bin = data["probs_per_bin"]
         except (IOError, KeyError) as e:
             print("ERROR - can't load numpy archive {}".format(file_name))
             print(e)

utils/dslices/exper_handler.py
@@ -100,7 +100,7 @@ class ExperimentHandler(object):
         std_auc_roc = np.std(self.aucs_roc)
         self.stats_auc_roc = tuple((mean_auc_roc, std_auc_roc))
 
-    def run_eval(self, data_set, model, do_balance=False, verbose=False):
+    def run_eval(self, data_set, model, do_balance=False, keep_features=False, verbose=False):
         self.reset_eval_metrics()
         eval_set_size = len(data_set.get_patient_ids(is_train=False))
         eval_batch = BatchHandlerSD(data_set=data_set, is_train=False, cuda=self.exper.run_args.cuda)
@@ -112,7 +112,7 @@ class ExperimentHandler(object):
                 #                                        the batch_size.
                 # batch_size=None and do_balance=False => classify all slices for a particular patient
                 x_input, y_labels, _ = eval_batch(batch_size=None, backward_freq=1, do_balance=do_balance)
-                eval_loss, pred_probs = model.do_forward_pass(x_input, y_labels)
+                eval_loss, pred_probs = model.do_forward_pass(x_input, y_labels, keep_features=keep_features)
             pred_labels = np.argmax(pred_probs.data.cpu().numpy(), axis=1)
             np_pred_probs = pred_probs.data.cpu().numpy()
             f1, roc_auc, pr_auc, prec, rec, fpr, tpr, precision, recall = \
@@ -176,7 +176,7 @@ class ExperimentHandler(object):
         # self.logger.info("\t Check: roc_auc={:.3f} - pr_auc={:.3f}".format(arr_val_eval[1], arr_val_eval[2]))
         self.reset_eval_metrics()
 
-    def test(self, data_set=None, model=None, test_id=None, verbose=False):
+    def test(self, data_set=None, model=None, test_id=None, keep_features=False, verbose=False):
         if model is None:
             # get model. 1st arg=experiment label "20180824_13_06_44_sdvgg11_bn_f1p01_brier_umap_6KE_lr1e05"
             #            2nd arg=last epoch id aka checkpoint
@@ -188,7 +188,7 @@ class ExperimentHandler(object):
         start_time = time.time()
         self.info("INFO - Begin test run {}".format(test_id))
         test_stats = {}
-        self.run_eval(data_set=data_set, model=model, do_balance=False, verbose=verbose)
+        self.run_eval(data_set=data_set, model=model, do_balance=False, keep_features=keep_features, verbose=verbose)
         test_stats["loss"] = self.eval_loss
         test_stats["f1"] = self.arr_eval_metrics[0]
         test_stats["roc_auc"] = self.arr_eval_metrics[1]
@@ -383,6 +383,10 @@ class ExperimentHandler(object):
 
 
 class ExperHandlerEnsemble(object):
+    """
+        Object that holds the ExperimentHandlers from the previous Segmentation task, NOT the slice detection
+        but we need those handlers (for u-maps, e-maps, results)
+    """
 
     def __init__(self, exper_dict):
         self.seg_exper_handlers = {}
@@ -418,3 +422,77 @@ class ExperHandlerEnsemble(object):
         ref_result_obj = ReferralResults(self.exper_dict, [referral_threshold], print_results=False,
                                          fold=None, slice_filter_type=None, use_entropy_maps=use_entropy_maps)
         self.dice_score_slices = ref_result_obj.org_dice_slices[referral_threshold]
+
+
+def get_experiment_handlers(root_dir, load_fold_id, seg_exper_ensemble=None):
+    """
+    Get slice detection handlers (experiments) for evaluation purposes
+
+    :param root_dir:
+    :param load_fold_id:
+    :param seg_exper_ensemble
+    :return:
+    """
+    log_dir = os.path.join(root_dir, "logs")
+
+    # Level 2 exper dicts
+    emap_exp_f0, emap_exp_f1, emap_exp_f2, emap_exp_f3 = {}, {}, {}, {}
+    umap_exp_f0, umap_exp_f1, umap_exp_f2, umap_exp_f3 = {}, {}, {}, {}
+    no_emap_exp_f0, no_emap_exp_f1, no_emap_exp_f2, no_emap_exp_f3 = {}, {}, {}, {}
+    no_umap_exp_f0, no_umap_exp_f1, no_umap_exp_f2, no_umap_exp_f3 = {}, {}, {}, {}
+    # Level 1 exper dicts
+    exp_emaps = {0: emap_exp_f0, 1: emap_exp_f1, 2: emap_exp_f2, 3: emap_exp_f3}
+    exp_umaps = {0: umap_exp_f0, 1: umap_exp_f1, 2: umap_exp_f2, 3: umap_exp_f3}
+    exp_no_emaps = {0: no_emap_exp_f0, 1: no_emap_exp_f1, 2: no_emap_exp_f2, 3: no_emap_exp_f3}
+    exp_no_umaps = {0: no_umap_exp_f0, 1: no_umap_exp_f1, 2: no_umap_exp_f2, 3: no_umap_exp_f3}
+    # ------------------------  FOLD 0 ------------------------
+    # tuple key is: epochs, batch_size, backward_freq, weight_decay, lambda, lr
+    #           ------------------------  e-maps ------------------------
+    emap_exp_f0[tuple((3000, 8, 10, 5, 7, 0.0001))] = "20180829_17_45_49_sdvgg11_bn_f0p01_brier_emap_3KE_lr1e04"
+    no_emap_exp_f0[tuple((3000, 8, 10, 5, 7, 0.0001))] = "20180829_17_55_47_sdvgg11_bn_f0p01_brier_noemap_3KE_lr1e04"
+    #           ------------------------ u-maps ------------------------
+    umap_exp_f0[tuple((3000, 8, 10, 5, 7, 0.0001))] = "20180829_18_05_55_sdvgg11_bn_f0p01_brier_umap_3KE_lr1e04"
+    no_umap_exp_f0[tuple((3000, 8, 10, 5, 7, 0.0001))] = "20180830_08_00_25_sdvgg11_bn_f0p01_brier_noumap_3KE_lr1e04"
+    # ------------------------ FOLD 1 -----------------------
+    #           ------------------------  e-maps ------------------------
+    emap_exp_f1[tuple((3000, 8, 10, 5, 7, 0.0001))] = "20180830_09_50_57_sdvgg11_bn_f1p01_brier_emap_3KE_lr1e04"
+    no_emap_exp_f1[tuple((3000, 8, 10, 5, 7, 0.0001))] = "20180830_10_00_39_sdvgg11_bn_f1p01_brier_noemap_3KE_lr1e04"
+
+    # FOLD 2
+    #           ------------------------  e-maps ------------------------
+    emap_exp_f2[tuple((3000, 8, 10, 5, 7, 0.0001))] = "20180830_09_41_07_sdvgg11_bn_f2p01_brier_emap_3KE_lr1e04"
+    no_emap_exp_f2[tuple((3000, 8, 10, 5, 7, 0.0001))] = "20180830_10_10_17_sdvgg11_bn_f2p01_brier_noemap_3KE_lr1e04"
+
+    # ------------------------ FOLD 3 ------------------------
+    #           ------------------------  e-maps ------------------------
+    emap_exp_f3[tuple((3000, 8, 10, 5, 7, 0.0001))] = "20180830_09_22_24_sdvgg11_bn_f3p01_brier_emap_3KE_lr1e04"
+    no_emap_exp_f3[tuple((3000, 8, 10, 5, 7, 0.0001))] = "20180830_09_31_51_sdvgg11_bn_f3p01_brier_noemap_3KE_lr1e04"
+
+    expers = [exp_emaps, exp_no_emaps, exp_umaps, exp_no_umaps]
+    # We also need four handler dictionaries
+    exp_hdl_emap, exp_hdl_no_emap, exp_hdl_umap, exp_hdl_no_umap = {}, {}, {}, {}
+    print("IMPORTANT INFO - Config key consists of #epoch, batch_size, backward_freq, weight_decay, lambda, lr")
+    for exper_dict_l1 in expers:
+        for fold_id, exper_dict_l2 in exper_dict_l1.iteritems():
+            if fold_id == load_fold_id:
+                for config_key, exper_id in exper_dict_l2.iteritems():
+                    print("")
+                    print("----------- Load experiment {} ---------------".format(exper_id))
+                    exp_path = os.path.join(log_dir, exper_id)
+                    exper_hdl = ExperimentHandler()
+                    exper_hdl.set_seg_ensemble(seg_exper_ensemble)
+                    exper_hdl.load_experiment(exp_path, use_logfile=False, verbose=False)
+                    exper_hdl.set_root_dir(root_dir)
+                    print("Model name: {} / config_key {}".format(exper_hdl.model_name, config_key))
+                    if exper_hdl.exper.run_args.type_of_map == "u_map":
+                        if exper_hdl.exper.run_args.use_no_map:
+                            exp_hdl_no_umap[config_key] = exper_hdl
+                        else:
+                            exp_hdl_umap[config_key] = exper_hdl
+                    else:
+                        if exper_hdl.exper.run_args.use_no_map:
+                            exp_hdl_no_emap[config_key] = exper_hdl
+                        else:
+                            exp_hdl_emap[config_key] = exper_hdl
+
+    return exp_hdl_emap, exp_hdl_no_emap, exp_hdl_umap, exp_hdl_no_umap